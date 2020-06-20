af9f32fc3150a99d3cb4a482b0ec02f4eb19c025
kaffeeanna <anna.antony@protonmail.com>
2020-06-20 17:38:17 +0200

    checkbox displays text (wip)

--- js/entscheidungsgallery.js
+++ js/entscheidungsgallery.js
@@ -10,3 +10,29 @@ var span = document.getElementsByClassName("close")[0];
 span.onclick = function() {
     modal.style.display = "none";
 };
+
+function checkgps() {
+    var checkBox = document.getElementById("gps");
+    var text = document.getElementById("gpstext");
+    if (checkBox.checked == true) {
+        text.style.display = "block";
+    } else {
+        text.style.display = "none";
+    }
+
+    var checkBox2 = document.getElementById("datenhinterlegung");
+    var text2 = document.getElementById("datenhinterlegungtext");
+    if (checkBox2.checked == true) {
+        text2.style.display = "block";
+    } else {
+        text2.style.display = "none";
+    }
+
+    var checkBox3 = document.getElementById("keins");
+    var text3 = document.getElementById("keintrackingtext");
+    if (checkBox3.checked == true) {
+        text3.style.display = "block";
+    } else {
+        text3.style.display = "none";
+    }
+}
