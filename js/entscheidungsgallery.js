755c496fcb6a28b989879b4e11f6021bb992b1ce
kaffeeanna <anna.antony@protonmail.com>
2020-06-21 15:00:34 +0200

    after 4 checked checkboxes show button

--- js/entscheidungsgallery.js
+++ js/entscheidungsgallery.js
@@ -170,3 +170,21 @@ function checkkameras() {
         text13.style.display = "none";
     }
 }
+
+function checkboxes() {
+    var inputElems = document.getElementsByTagName("input"),
+        count = 0;
+
+    for (var i = 0; i < inputElems.length; i++) {
+        if (inputElems[i].type == "checkbox" && inputElems[i].checked == true) {
+            count++;
+            console.log(count);
+        }
+    }
+    var woche1 = document.getElementById("weiter");
+    if (count == 4) {
+        woche1.style.display = "block";
+    } else {
+        woche1.style.display = "none";
+    }
+}
