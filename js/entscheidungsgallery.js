e628ff93c9c0ddf9012f09269cfc2a560efc345c
HilalYaz <45353212+HilalYaz@users.noreply.github.com>
2020-07-06 01:19:00 +0200

    test because new Brench

--- js/entscheidungsgallery.js
+++ js/entscheidungsgallery.js
@@ -8,16 +8,6 @@ if (localStorage.getItem("scoreAmount") === null) {
   localStorage.setItem("scoreAmount", "0");
 }
 
-var decisionsMade = localStorage.getItem("decisionsAmount");
-if (localStorage.getItem("decisionsAmount") === null) {
-  localStorage.setItem("decisionsAmount", "0");
-}
-
-if (currentWeek >= 2) {
-  var postits = document.getElementById("postits");
-  postits.style.display = "block";
-}
-
 // Postit White
 
 var postitmodal1 = document.getElementById("PostIt1Modal");
@@ -142,115 +132,122 @@ function checkPaper(keys, elementId) {
     document.getElementById(elementId).style.display = "none";
 }
 
-checkPaper(["gps", "personaldata", "notracking"], "myImg");
-checkPaper(["healthinsurance", "nohealthinsurance"], "myImg2");
-checkPaper(["corporation", "nocorporation"], "myImg3");
-checkPaper(["travel", "notravel"], "myImg4");
+checkPaper(["gps", "datenhinterlegung", "keintracking"], "myImg");
+checkPaper(["jakrankenkasse", "keinkks"], "myImg2");
+checkPaper(["jakonzerne", "keinkonzerne"], "myImg3");
+checkPaper(["jareise", "keinreise"], "myImg4");
 checkPaper(
   [
-    "cameras",
-    "facialrecognition",
-    "thermalimage",
-    "thermalimagefacialrecognition",
-    "nocameras",
+    "jakameras",
+    "jagesichtserkennung",
+    "jawaermebild",
+    "waermebildgesichtserkennung",
+    "keinkameras",
   ],
   "myImg5"
 );
-checkPaper(["realestate", "norealestate"], "myImg6");
-checkPaper(["jobfield", "nojobfield"], "myImg7");
-checkPaper(["deposit", "nodeposit"], "myImg8");
+checkPaper(["yesrealestate", "norealestate"], "myImg6");
+checkPaper(["yesjobfield", "nojobfield"], "myImg7");
+checkPaper(["yesdeposit", "nodeposit"], "myImg8");
 checkPaper(
-  ["cybertracking", "cybertrackingandscore", "nocybertracking"],
+  ["yescybertracking", "cybertrackingandscore", "nocybertracking"],
   "myImg9"
 );
-checkPaper(["vpn", "novpn"], "myImg10");
-checkPaper(["pets", "nopets"], "myImg11");
-checkPaper(["securitypolice", "nosecurity"], "myImg12");
-checkPaper(["education", "noeducation"], "myImg13");
-checkPaper(["loans", "noloans"], "myImg14");
-checkPaper(["blacklist", "noblacklists"], "myImg15");
-checkPaper(["bureaucracy", "nobureaucracy"], "myImg16");
+checkPaper(["yesvpn", "novpn"], "myImg10");
+checkPaper(["yespets", "nopets"], "myImg11");
+checkPaper(["yessecurity", "nosecurity"], "myImg12");
+checkPaper(["yeseducation", "noeducation"], "myImg13");
+checkPaper(["yesloan", "noloan"], "myImg14");
+checkPaper(["yesblacklist", "noblacklist"], "myImg15");
+checkPaper(["yesbureaucracy", "nobureaucracy"], "myImg16");
 
 var button1 = new CheckBox("gps", "gpstext", "gps", "gpspost", -10);
 var button2 = new CheckBox(
-  "personaldata",
-  "personaldatatext",
-  "personaldata",
-  "personaldatapost",
+  "datenhinterlegung",
+  "datenhinterlegungtext",
+  "datenhinterlegung",
+  "datenpost",
   -5
 );
-var button3 = new CheckBox("notracking", "notrackingtext", "notracking", "", 1);
+var button3 = new CheckBox("keins", "keintrackingtext", "keintracking", "", 1);
 
 button1.initEventListener([button2, button3]);
 button2.initEventListener([button1, button3]);
 button3.initEventListener([button2, button1]);
 
-var button4 = new CheckBox(
-  "nohealthinsurance",
-  "nohealthinsurancetext",
-  "nohealthinsurance",
-  "",
-  1
-);
+var button4 = new CheckBox("neinkrankenkasse", "neinkkstext", "keinkks", "", 1);
 var button5 = new CheckBox(
-  "healthinsurance",
-  "healthinsurancetext",
-  "healthinsurance",
-  "healthinsurancepost",
+  "jakrankenkasse",
+  "jakkstext",
+  "jakrankenkasse",
+  "krankenkassepost",
   -3
 );
 button4.initEventListener([button5]);
 button5.initEventListener([button4]);
 
 var button6 = new CheckBox(
-  "nocorporation",
-  "nocorporationtext",
-  "nocorporation",
+  "neinkonzerne",
+  "neinkonzernetext",
+  "keinkonzerne",
   "",
   1
 );
+
 var button7 = new CheckBox(
-  "corporation",
-  "corporationtext",
-  "corporation",
-  "corporationpost",
+  "jakonzerne",
+  "jakonzernetext",
+  "jakonzerne",
+  "konzernepost",
   -10
 );
 button6.initEventListener([button7]);
 button7.initEventListener([button6]);
 
-var button8 = new CheckBox("notravel", "notraveltext", "notravel", "", 1);
-var button9 = new CheckBox("travel", "traveltext", "travel", "travelpost", -7);
+var button8 = new CheckBox("neinreise", "neinreisetext", "keinreise", "", 1);
+var button9 = new CheckBox(
+  "jareise",
+  "jareisetext",
+  "jareise",
+  "reisepost",
+  -7
+);
 button8.initEventListener([button9]);
 button9.initEventListener([button8]);
 
-var button10 = new CheckBox("nocameras", "nocamerastext", "nocameras", "", 1);
+var button10 = new CheckBox(
+  "neinkameras",
+  "neinkamerastext",
+  "keinkameras",
+  "",
+  1
+);
 var button11 = new CheckBox(
-  "facialrecognition",
-  "facialrecognitiontext",
-  "facialrecognition",
-  "facialrecognitionpost",
+  "jagesichtserkennung",
+  "jagesichtserkennungtext",
+  "jagesichtserkennung",
+  "gesichtserkennungpost",
   -6
 );
 var button12 = new CheckBox(
-  "thermalimage",
-  "thermalimagetext",
-  "thermalimage",
-  "thermalimagepost",
+  "jawaermebild",
+  "jawaermebildtext",
+  "jawaermebild",
+  "waermebildpost",
   -8
 );
 var button13 = new CheckBox(
-  "thermalimagefacialrecognition",
-  "thermalimagefacialrecognitiontext",
-  "thermalimagefacialrecognition",
-  "thermalimagefacialrecognitionpost",
+  "waermebildgesichtserkennung",
+  "waermebildgesichtserkennungtext",
+  "waermebildgesichtserkennung",
+  "waermebildgesichtserkennungpost",
   -10
 );
 var button14 = new CheckBox(
-  "cameras",
-  "camerastext",
-  "cameras",
-  "cameraspost",
+  "jakameras",
+  "jakamerastext",
+  "jakameras",
+  "kamerapost",
   -9
 );
 button10.initEventListener([button11, button12, button13, button14]);
@@ -348,7 +345,7 @@ var button28 = new CheckBox(
 );
 var button29 = new CheckBox(
   "nosecurity",
-  "nosecuritytext",
+  "nosecuritystext",
   "nosecurity",
   "",
   1
@@ -606,3 +603,247 @@ var span16 = document.getElementsByClassName("close16")[0];
 span16.onclick = function () {
   modal16.style.display = "none";
 };
+
+function checkboxes() {
+  var inputElems = document.getElementsByTagName("input"),
+    count = 0;
+
+  for (var i = 0; i < inputElems.length; i++) {
+    if (inputElems[i].type == "radio" && inputElems[i].checked == true) {
+      count++;
+    }
+  }
+
+  var woche1 = document.getElementById("weiter");
+  if (count == 4) {
+    woche1.style.display = "block";
+  } else {
+    woche1.style.display = "none";
+  }
+}
+
+//Bar
+
+//button1
+const gps = document.getElementById("gps");
+gps.addEventListener("click", () => {
+  health.value -= 10;
+});
+
+//button2
+const datenhinterlegung = document.getElementById("datenhinterlegung");
+datenhinterlegung.addEventListener("click", () => {
+  health.value -= 5;
+});
+
+//button3
+const keins = document.getElementById("keins");
+keins.addEventListener("click", () => {
+  health.value += 1;
+});
+
+//button4
+const neinkrankenkasse = document.getElementById("neinkrankenkasse");
+neinkrankenkasse.addEventListener("click", () => {
+  health.value += 1;
+});
+
+//button5
+const jakrankenkasse = document.getElementById("jakrankenkasse");
+jakrankenkasse.addEventListener("click", () => {
+  health.value -= 3;
+});
+
+//button6
+const neinkonzerne = document.getElementById("neinkonzerne");
+neinkonzerne.addEventListener("click", () => {
+  health.value += 1;
+});
+
+//button7
+const jakonzerne = document.getElementById("jakonzerne");
+jakonzerne.addEventListener("click", () => {
+  health.value -= 10;
+});
+
+//button8
+const neinreise = document.getElementById("neinreise");
+neinreise.addEventListener("click", () => {
+  health.value += 1;
+});
+
+//button9
+const jareise = document.getElementById("jareise");
+jareise.addEventListener("click", () => {
+  health.value -= 7;
+});
+
+//button10
+const neinkameras = document.getElementById("neinkameras");
+neinkameras.addEventListener("click", () => {
+  health.value += 1;
+});
+
+//button11
+const jagesichtserkennung = document.getElementById("jagesichtserkennung");
+jagesichtserkennung.addEventListener("click", () => {
+  health.value -= 6;
+});
+
+//button12
+const jawaermebild = document.getElementById("jawaermebild");
+jawaermebild.addEventListener("click", () => {
+  health.value -= 8;
+});
+
+//button13
+const waermebildgesichtserkennung = document.getElementById(
+  "waermebildgesichtserkennung"
+);
+waermebildgesichtserkennung.addEventListener("click", () => {
+  health.value -= 10;
+});
+
+//button14
+const jakameras = document.getElementById("jakameras");
+jakameras.addEventListener("click", () => {
+  health.value -= 9;
+});
+
+//button15
+const yesrealestate = document.getElementById("yesrealestate");
+yesrealestate.addEventListener("click", () => {
+  health.value -= 2;
+});
+
+//button16
+const norealestate = document.getElementById("norealestate");
+norealestate.addEventListener("click", () => {
+  health.value += 1;
+});
+
+//button17
+const yesjobfield = document.getElementById("yesjobfield");
+yesjobfield.addEventListener("click", () => {
+  health.value += 3;
+});
+
+//button18
+const nojobfield = document.getElementById("nojobfield");
+nojobfield.addEventListener("click", () => {
+  health.value += 1;
+});
+
+//button19
+const yesdeposit = document.getElementById("yesdeposit");
+yesdeposit.addEventListener("click", () => {
+  health.value += 6;
+});
+
+//button20
+const nodeposit = document.getElementById("nodeposit");
+nodeposit.addEventListener("click", () => {
+  health.value += 1;
+});
+
+//button21
+const yescybertracking = document.getElementById("yescybertracking");
+yescybertracking.addEventListener("click", () => {
+  health.value -= 9;
+});
+
+//button22
+const cybertrackingandscore = document.getElementById("cybertrackingandscore");
+cybertrackingandscore.addEventListener("click", () => {
+  health.value -= 10;
+});
+
+//button23
+const nocybertracking = document.getElementById("nocybertracking");
+nocybertracking.addEventListener("click", () => {
+  health.value += 1;
+});
+
+//button24
+const yesvpn = document.getElementById("yesvpn");
+yesvpn.addEventListener("click", () => {
+  health.value -= 7;
+});
+
+//button25
+const novpn = document.getElementById("novpn");
+novpn.addEventListener("click", () => {
+  health.value += 1;
+});
+
+//button26
+const yespets = document.getElementById("yespets");
+yespets.addEventListener("click", () => {
+  health.value -= 3;
+});
+
+//button27
+const nopets = document.getElementById("nopets");
+nopets.addEventListener("click", () => {
+  health.value += 1;
+});
+
+//button28
+const yessecurity = document.getElementById("yessecurity");
+yessecurity.addEventListener("click", () => {
+  health.value -= 7;
+});
+
+//button29
+const nosecurity = document.getElementById("nosecurity");
+nosecurity.addEventListener("click", () => {
+  health.value += 1;
+});
+
+//button30
+const yeseducation = document.getElementById("yeseducation");
+yeseducation.addEventListener("click", () => {
+  health.value -= 4;
+});
+
+//button31
+const noeducation = document.getElementById("noeducation");
+noeducation.addEventListener("click", () => {
+  health.value += 1;
+});
+
+//button32
+const yesloan = document.getElementById("yesloan");
+yesloan.addEventListener("click", () => {
+  health.value -= 5;
+});
+
+//button33
+const noloan = document.getElementById("noloan");
+noloan.addEventListener("click", () => {
+  health.value += 1;
+});
+
+//button34
+const yesblacklist = document.getElementById("yesblacklist");
+yesblacklist.addEventListener("click", () => {
+  health.value -= 4;
+});
+
+//button35
+const noblacklist = document.getElementById("noblacklist");
+noblacklist.addEventListener("click", () => {
+  health.value += 1;
+});
+
+//button36
+const yesbureaucracy = document.getElementById("yesbureaucracy");
+yesbureaucracy.addEventListener("click", () => {
+  health.value -= 3;
+});
+
+//button37
+const nobureaucracy = document.getElementById("nobureaucracy");
+nobureaucracy.addEventListener("click", () => {
+  health.value += 1;
+});
